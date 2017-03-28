9aec032e8ae2b271aa91fbebc28179f201e3b04a
Hieu Nguyen <Hieu Nguyen>
2017-03-28 23:33:23 +0700

    Init project

---
+++ HQT/HQT/Login.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace HQT
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grbLogin = new System.Windows.Forms.GroupBox();
+            this.ckRemember = new System.Windows.Forms.CheckBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnAskRegister = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbConfirmPassword = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.grbLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(106, 25);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(161, 20);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(106, 65);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(161, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // grbLogin
+            // 
+            this.grbLogin.Controls.Add(this.lbConfirmPassword);
+            this.grbLogin.Controls.Add(this.txtConfirmPassword);
+            this.grbLogin.Controls.Add(this.btnAskRegister);
+            this.grbLogin.Controls.Add(this.btnLogin);
+            this.grbLogin.Controls.Add(this.ckRemember);
+            this.grbLogin.Controls.Add(this.txtUsername);
+            this.grbLogin.Controls.Add(this.label2);
+            this.grbLogin.Controls.Add(this.txtPassword);
+            this.grbLogin.Controls.Add(this.label1);
+            this.grbLogin.Location = new System.Drawing.Point(45, 173);
+            this.grbLogin.Name = "grbLogin";
+            this.grbLogin.Size = new System.Drawing.Size(317, 172);
+            this.grbLogin.TabIndex = 4;
+            this.grbLogin.TabStop = false;
+            this.grbLogin.Text = "Login";
+            // 
+            // ckRemember
+            // 
+            this.ckRemember.AutoSize = true;
+            this.ckRemember.Location = new System.Drawing.Point(106, 100);
+            this.ckRemember.Name = "ckRemember";
+            this.ckRemember.Size = new System.Drawing.Size(77, 17);
+            this.ckRemember.TabIndex = 4;
+            this.ckRemember.Text = "Remember";
+            this.ckRemember.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(106, 128);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnAskRegister
+            // 
+            this.btnAskRegister.Location = new System.Drawing.Point(192, 128);
+            this.btnAskRegister.Name = "btnAskRegister";
+            this.btnAskRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnAskRegister.TabIndex = 6;
+            this.btnAskRegister.Text = "Register";
+            this.btnAskRegister.UseVisualStyleBackColor = true;
+            this.btnAskRegister.Click += new System.EventHandler(this.btnAskRegister_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Help;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(107, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(186, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lbConfirmPassword
+            // 
+            this.lbConfirmPassword.AutoSize = true;
+            this.lbConfirmPassword.Location = new System.Drawing.Point(6, 101);
+            this.lbConfirmPassword.Name = "lbConfirmPassword";
+            this.lbConfirmPassword.Size = new System.Drawing.Size(90, 13);
+            this.lbConfirmPassword.TabIndex = 8;
+            this.lbConfirmPassword.Text = "Confirm password";
+            this.lbConfirmPassword.Visible = false;
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(106, 98);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.PasswordChar = '*';
+            this.txtConfirmPassword.Size = new System.Drawing.Size(161, 20);
+            this.txtConfirmPassword.TabIndex = 7;
+            this.txtConfirmPassword.Visible = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(413, 382);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.grbLogin);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.grbLogin.ResumeLayout(false);
+            this.grbLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grbLogin;
+        private System.Windows.Forms.CheckBox ckRemember;
+        private System.Windows.Forms.Button btnAskRegister;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbConfirmPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+    }
+}
---
+++ HQT/HQT/Login.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HQT.Properties;
+
+namespace HQT
+{
+    public partial class Login : Form
+    {
+        private bool _isLogin;
+        public Login()
+        {
+            InitializeComponent();
+            txtConfirmPassword.Visible = false;
+            lbConfirmPassword.Visible = false;
+            ckRemember.Visible = true;
+            _isLogin = true;
+        }
+
+        private void btnAskRegister_Click(object sender, EventArgs e)
+        {
+            if (_isLogin)
+            {
+                ResetControl();
+                grbLogin.Text = Resources.RegisterTitle;
+                txtConfirmPassword.Visible = true;
+                lbConfirmPassword.Visible = true;
+                ckRemember.Visible = false;
+                _isLogin = false;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (_isLogin == false)
+            {
+                grbLogin.Text = Resources.LoginTitle;
+                ResetControl();
+                txtConfirmPassword.Visible = false;
+                lbConfirmPassword.Visible = false;
+                ckRemember.Visible = true;
+                _isLogin = true;
+            }
+        }
+
+        private void ResetControl()
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            txtConfirmPassword.Text = "";
+            ckRemember.Checked = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, Resources.TeamInformation, Resources.TeamName,
+                MessageBoxButtons.OK);
+        }
+    }
+}
